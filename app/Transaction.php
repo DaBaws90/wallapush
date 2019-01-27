2f3ba4788187fe97382f765ce7d462ed1d9076d4
DaBaws90 <43435682+DaBaws90@users.noreply.github.com>
2019-01-27 02:18:35 +0100

    Version 0.2.0
    Configured all relationships between models.

--- app/Transaction.php
+++ app/Transaction.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\anuncio;
+use App\User;
 
 class Transaction extends Model
 {
-    //
+    protected $table = "transactions";
+
+    protected $fillable = [];
+
+    public function anuncion(){
+        return $this->belongsTo(anuncio::class, 'id_anuncio');
+    }
+
+    public function comprador(){
+        return $this->belongsTo(User::class, 'id_comprador');
+    }
+
+    // public function vendedor(){}
 }
--- app/image.php
+++ app/image.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\anuncio;
 
 class Image extends Model
 {
-    //
+    protected $table = 'images';
+
+    protected $fillable = ['img'];
+
+    public function anuncio(){
+        return $this->belongsTo(anuncio::class, 'id_anuncio');
+    }
 }
