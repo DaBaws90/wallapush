eeb87c7a55a94d5f71d809aa306c65267d1a2189
DaBaws90 <43435682+DaBaws90@users.noreply.github.com>
2019-02-15 18:07:52 +0100

    Update app.blade.php
    Footer removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -134,14 +134,14 @@
         </nav>
 
         @yield('content')
-        <footer class="text-muted text-center text-small">
+        <!-- <footer class="text-muted text-center text-small">
             <p class="mb-1">&copy; 2018-2019 Wallapush</p>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="#">Privacy</a></li>
                 <li class="list-inline-item"><a href="#">Terms</a></li>
                 <li class="list-inline-item"><a href="#">Support</a></li>
             </ul>
-        </footer>
+        </footer> -->
     </div>
 </body>
 
